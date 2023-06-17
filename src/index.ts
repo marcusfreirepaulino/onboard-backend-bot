7deffbbfacccee3ac2221cae05869c6a9263fa40
marcusfreirepaulino <marcusfreirepaulino@gmail.com>
2023-06-17 12:21:33 -0300

    define tutorial resolver and run server

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
+const typeDefs = `
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "Hello World!",
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
