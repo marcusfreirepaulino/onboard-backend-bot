7f1a097d2f36b59d1d096cc0421d127ce2bd54d5
marcusfreirepaulino <marcusfreirepaulino@gmail.com>
2023-06-21 15:01:49 -0300

    removing id?

--- src/domain/user/create-user.use-case.ts
+++ src/domain/user/create-user.use-case.ts
@@ -5,7 +5,7 @@ import { UserModel } from '../../model';
 export async function createUserRow(data: UserModel) {
   const userRepository = AppDataSource.getRepository(User);
   const user = new User();
-  user.id = 1;
+
   user.name = data.name;
   user.email = data.email;
   user.birthDate = data.birthDate;
