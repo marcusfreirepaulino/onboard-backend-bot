747eb7f2cbcafff1c5abddca9f6d8ae256510762
marcusfreirepaulino <marcusfreirepaulino@gmail.com>
2023-06-22 11:21:37 -0300

    remove unencessary trycatch

--- src/domain/user/create-user.use-case.ts
+++ src/domain/user/create-user.use-case.ts
@@ -3,14 +3,23 @@ import { User } from '../../data/db';
 import { emailValidator, passwordValidator } from '../../data/validators';
 import { UserModel } from '../../model';
 
-export async function createUserUseCase(data: UserModel) {
+export async function createUserUseCase(input: UserModel) {
   const userRepository = AppDataSource.getRepository(User);
   const user = new User();
 
-  user.name = data.name;
-  user.email = data.email;
-  user.birthDate = data.birthDate;
-  user.password = data.password;
+  emailValidator(input.email);
+  passwordValidator(input.password);
+  const findEmail = await userRepository.findOneBy({ email: input.email });
 
-  return userRepository.save(user);
+  if (!!findEmail) {
+    throw new Error('This email is already registered.');
+  }
+
+  user.name = input.name;
+  user.email = input.email;
+  user.birthDate = input.birthDate;
+  user.password = input.password;
+
+  const data = await userRepository.save(user);
+  return data;
 }
