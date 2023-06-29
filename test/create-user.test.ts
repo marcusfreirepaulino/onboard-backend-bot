2d405fbe657d8c0c42682bff26d0bdd20689e065
marcusfreirepaulino <marcusfreirepaulino@gmail.com>
2023-06-29 14:24:20 -0300

    create custom erros

--- test/create-user.test.ts
+++ test/create-user.test.ts
@@ -107,4 +107,26 @@ describe('Create User', () => {
     const customError = response.errors[0] as CustomError;
     expect(!!customError.message && !!customError.code).to.be.true;
   });
+
+  it('should return custom error messages', async () => {
+    const invalidPasswordInput = {
+      data: {
+        ...variables.data,
+        password: '123',
+      },
+    };
+
+    const { data: response } = await axios({
+      url: endpoint,
+      method: 'post',
+      headers: headers,
+      data: {
+        variables: invalidPasswordInput,
+        query: mutation,
+      },
+    });
+
+    const customError = response.errors[0] as CustomError;
+    expect(!!customError.message && !!customError.code).to.be.true;
+  });
 });
